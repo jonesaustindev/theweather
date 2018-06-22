620436995a37b016a428e03f4d6b65a9db251d9a
Austin Jones <jonesaustindev@gmail.com>
2018-06-21 19:59:58 -0500

    reorg folder structure

--- theweather-server/app.js
+++ theweather-server/app.js
@@ -17,8 +17,14 @@ apiKey = process.env.API_KEY;
 // port //
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 // routes //
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/api', 
     searchRoutes
 );
--- theweather-server/routes/search.js
+++ theweather-server/routes/search.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { search } = require('../handlers/search');
 
-router.get('/search', search);
+router.get('/search', function(req, res){
+    res.sendFile(path.join(__dirname, 'build', search));
+});
 
 module.exports = router;
